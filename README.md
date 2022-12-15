b4e393bcf6a31c72d3ab05ceb1d71590a0d25a58
unknown <schlae@users.noreply.github.com>
2022-12-15 09:58:30 -0800

    Initial version.

---
+++ README.md
@@ -0,0 +1,63 @@
+# Replica Datanetics Keyboard
+
+Do you need a Datanetics ASCII keyboard for your computer project (such as a replica Apple 1)? Don't want to pay high prices for an original Datanetics keyboard? Then consider building this replica.
+
+![Replica Datanetics keyboard photo](https://github.com/schlae/replica-datanetics/blob/main/images/replica-datanetics.jpg)
+
+This is not the first replica Datanetics keyboard. Mike Willegal [designed one](http://www.willegal.net/appleii/apple1-datanetics.htm). In fact, I started with his design and redid it using KiCad. Instead of using the expensive, fragile, and hard-to-find MM5740 keyboard chip, I switched to a more common microcontroller: the ATMega644P. I also removed the output buffer chips--just in case you want to redo my firmware and implement some other keyboard protocol, you'll be able to control the pin direction.
+
+Here are the design files. The BOM does not include manufacturer part numbers because it uses very common through hole parts that you might already have. The key switches are standard Cherry MX and are available from a wide variety of sources.
+
+[Schematic](https://github.com/schlae/replica-datanetics/blob/main/Apple1Keyb.pdf)
+
+[Bill of Materials](https://github.com/schlae/replica-datanetics/blob/main/Apple1Keyb.csv)
+
+[Fab Files](https://github.com/schlae/replica-datanetics/blob/main/fab/Apple1Keyb_Rev1.zip)
+
+[Water Slide Decal Graphics](https://github.com/schlae/replica-datanetics/blob/main/keycaps.svg)
+
+## Fabrication and Assembly Notes
+
+The circuit board is a 2-layer board, 11.75 by 5.5 inches (298.5 x 139.7mm). No special edge connector plating or bevel is needed. To make the board look like the original, order the board "bare bones" with no solder mask or silkscreen.
+
+**Caution: There is a "matrix" via area in the upper left corner of the board with 76 non-plated vias. The drill locations for these are included in the "Apple1Keyb-NPTH.drl" file in the fab package. The User Comments layer also includes a note to the fab vendor to NOT plate these holes. You may want to add an extra comment to your fab vendor when you put in the order that these 76 holes must not be plated.**
+
+To assemble the board, just solder in the components. I'd recommend using a socket for the U1 microcontroller so that you can reprogram it later. The NE555 timer (U2) in the original design was meant to provide the clock for the MM5740 chip, but isn't necessary in this design. You can install it or leave it out.
+
+The second NE555 timer (U3) is necessary for the key repeat function.
+
+## Key Caps
+
+The keyboard design uses standard Cherry MX key switches and keycaps are available from numerous sources. For my own build, I used DSA keycaps along with an 8U spacebar from PMK. I looked into a number of ways of putting the legends on the keycaps but I ended up having some water slide decals custom made. See the file above if you want to use this same file, but I'd recommend putting the legends closer together so they'll fit on a smaller sheet, saving you money.
+
+## Matrix Via Area
+
+The Datanetics design has four key switches that are not connected to the key scanning matrix and are brought out to individual pins on the edge connector. If you're building an Apple 1, you'll want to use two of these keys for the reset and clear screen functions. 
+
+If you want keys to be included in the scanning matrix, you'll want to solder a fine wire through the appropriate via in the upper left corner of the board. To find out which key will be generated, look up the row and column number in the key\_rom array in firmware source code. You can, of course, customize the generated key by editing the source and recompiling.
+
+## Firmware
+
+The firmware is designed for the ATMega644P microcontroller, but it should be possible to modify it to work with other 40-pin microcontrollers in the ATMega family. The prebuilt firmware behaves like a stock MM5740.
+
+[Prebuilt Firmware](https://github.com/schlae/replica-datanetics/blob/main/fw/apple1keyb.hex)
+
+Be sure to set the fuse bits of the ATMega644P according to the fuses.conf file:
+
+```
+lfuse = 0x62
+hfuse = 0xdd
+efuse = 0xfc
+```
+
+You can build the firmware yourself pretty easily. Under Ubuntu, install the `gcc-avr` package, and build the firmware by going to the directory and typing `make`.
+
+If you have a minipro programmer (TL866 etc) you can program the device by typing `make fuses` followed by `make prog`. For this to work correctly, you'll need the [minipro software](https://gitlab.com/DavidGriffith/minipro) installed. You could also use the Windows minipro software from the manufacturer, or another programmer entirely.
+
+## Mechanical Brackets
+
+[Mike Willegal's site](http://www.willegal.net/appleii/apple1-datanetics.htm) has drawings for a mechanical bracket.
+
+## License
+This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
+International License. See [https://creativecommons.org/licenses/by-sa/4.0/](https://creativecommons.org/licenses/by-sa/4.0/).
---
+++ fw/main.c
@@ -0,0 +1,236 @@
+/* MM5740 equivalent clone, copyright (C) 2022 Tube Time.
+
+   This work is licensed under the Creative Commons Attribution-ShareAlike 4.0
+   International License. To view a copy of this license, visit
+   http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to
+   Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+*/
+
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+/* Port assignments:
+    PORTA = External data port (outputs)
+    PORTB
+        0 - X9
+        1 - X8
+        2 - X7
+        3 - X6
+        4 - X5
+        5 - X4
+        6 - X3
+        7 - X2
+    PORTC
+        0 - Y2
+        1 - Y3
+        2 - Y4
+        3 - Y5
+        4 - Y6
+        5 - Y7
+        6 - Y8
+        7 - Y9
+    PORTD
+        0 - X1
+        1 - SRB (strobe output)
+        2 - Repeat clock in
+        3 - n/c
+        4 - Control key
+        5 - Shift key
+        6 - Y10
+        7 - Y1
+*/
+
+#define KEY_COUNT           90  // Total number of possible keys
+#define DEBOUNCE_DELAY     200  // 2ms debounce delay
+#define STROBE_TIME         50  // 50us strobe pulse
+#define ROW_SETTLING_TIME   50  // 50us settling time
+
+bool keyflags[KEY_COUNT] = {0};
+bool repeat_history = false;
+
+uint8_t key_rom[] = {
+// Row
+// 1     2     3     4     5     6     7     8     9   10
+// Unshifted
+ '8',  '4',  '5',  '1',  '2',  '3',  '0',  '6',  '9', '7', // Col 1
+0x0c, 0x0d, 0x1c, 0x1d, 0x0b, 0x0e, 0x20, 0x09, 0x08, '-', // Col 2
+ '0', 0x0a,  'P', 0x7f,  ';',  '.',  '/',  'P',  'O', ':', // Col 3
+ '9',  'I',  'O',  'K',  'L',  ',',  '.',  'L',  'K', '8', // Col 4
+ '6',  'U',  'Y',  'J',  'H',  'M',  'N',  'M',  'N', '7', // Col 5
+ '5',  'R',  'T',  'F',  'G',  'V',  'B', 0x18, 0x19, '4', // Col 6
+0x12,  'E', 0x13,  'D', 0x14,  'C', 0x15, 0x16, 0x17, '3', // Col 7
+0x05,  'W', 0x06,  'S', 0x07,  'X', 0x0f, 0x10, 0x11, '2', // Col 8
+0x00,  'Q', 0x1b,  'A', 0x01,  'Z', 0x02, 0x03, 0x04, '1', // Col 9
+
+// Shifted
+ '8',  '4',  '5',  '1',  '2',  '3',  '0',  '6',  '9', '7', // Col 1
+0x0c, 0x0d, 0x1c, 0x1d, 0x0b, 0x0e, 0x20, 0x09, 0x08, '-', // Col 2
+ '0', 0x0a,  '@', 0x7f,  '+',  '.',  '?',  'P',  'O', '*', // Col 3
+ ')',  'I',  '_',  '[', 0x5c,  '<',  '>',  'L',  'K', '(', // Col 4
+ '&',  'U',  'Y',  'J',  'H',  ']',  '^',  'M',  'N',0x27, // Col 5
+ '%',  'R',  'T',  'F',  'G',  'V',  'B', 0x18, 0x19, '$', // Col 6
+0x12,  'E', 0x13,  'D', 0x14,  'C', 0x15, 0x16, 0x17, '#', // Col 7
+0x05,  'W', 0x06,  'S', 0x07,  'X', 0x0f, 0x10, 0x11,0x22, // Col 8
+0x00,  'Q', 0x1b,  'A', 0x01,  'Z', 0x02, 0x03, 0x04, '!', // Col 9
+
+// Control
+ '8',  '4',  '5',  '1',  '2',  '3',  '0',  '6',  '9', '7', // Col 1
+0x0c, 0x0d, 0x1c, 0x1d, 0x0b, 0x0e, 0x20, 0x09, 0x08, '-', // Col 2
+ '0', 0x0a,  'P', 0x7f,  ';',  '.',  '/', 0x10, 0x0f, ':', // Col 3
+ '9', 0x09, 0x0f, 0x0b, 0x0c,  ',',  '.', 0x0c, 0x0b, '8', // Col 4
+ '6', 0x15, 0x19, 0x0a, 0x08, 0x0d, 0x0e, 0x0d, 0x0e, '7', // Col 5
+ '5', 0x12, 0x14, 0x06, 0x07, 0x16, 0x02, 0x18, 0x19, '4', // Col 6
+0x12, 0x05, 0x13, 0x04, 0x14, 0x03, 0x15, 0x16, 0x17, '3', // Col 7
+0x05, 0x17, 0x06, 0x13, 0x07, 0x18, 0x0F, 0x10, 0x11, '2', // Col 8
+0x00, 0x11, 0x1b, 0x01, 0x01, 0x1a, 0x02, 0x03, 0x04, '1', // Col 9
+
+// Shift + Control
+ '8',  '4',  '5',  '1',  '2',  '3',  '0',  '6',  '9', '7', // Col 1
+0x0c, 0x0d, 0x1c, 0x1d, 0x0b, 0x0e, 0x20, 0x09, 0x08, '-', // Col 2
+ '0', 0x0a, 0x00, 0x7f,  '+',  '.',  '?', 0x10, 0x0f, '*', // Col 3
+ ')', 0x09, 0x1f, 0x1b, 0x1c,  '<',  '>', 0x0c, 0x0b, '(', // Col 4
+ '&', 0x15, 0x19, 0x0a, 0x08, 0x1d, 0x1e, 0x0d, 0x0e,0x27, // Col 5
+ '%', 0x12, 0x14, 0x06, 0x07, 0x16, 0x02, 0x18, 0x19, '$', // Col 6
+0x12, 0x05, 0x13, 0x04, 0x14, 0x03, 0x15, 0x16, 0x17, '#', // Col 7
+0x05, 0x17, 0x06, 0x13, 0x07, 0x18, 0x0F, 0x10, 0x11,0x22, // Col 8
+0x00, 0x11, 0x1b, 0x01, 0x01, 0x1a, 0x02, 0x03, 0x04, '!', // Col 9
+
+
+};
+
+// Output Bit changes for ROM:
+// 1, 2, 3, 4,  ,  ,  ,
+//             5, 6, 7, 8    - changes for ctrl and shift
+
+
+// Disables the drive signal on a X column
+void disable_column(uint8_t col)
+{
+    // Disable the current column
+    if (col == 0) {
+        DDRD &= ~_BV(0);
+    } else {
+        DDRB &= ~_BV(8 - col);
+    }
+}
+
+// Enables the drive signal on a X column
+void enable_column(uint8_t col)
+{
+    // Enable the next column
+    if (col == 0) {
+        DDRD |= _BV(0);
+    } else {
+        DDRB |= _BV(8 - col);
+    }
+}
+
+// Read the contents of the Y rows.
+uint16_t read_rows()
+{
+    return ~((((uint16_t)PIND & 0x40) << 3) |
+              ((uint16_t)PINC << 1) |
+              ((uint16_t)PIND >> 7));
+}
+
+// Pulse the strobe output
+void strobe()
+{
+    PORTD |= _BV(1);
+    _delay_us(STROBE_TIME);
+    PORTD &= ~_BV(1);
+}
+
+// Checks for falling edge on the key repeat signal
+bool is_repeat()
+{
+    bool repeat = (PIND & _BV(2));
+    if (!repeat && repeat_history) {
+        repeat_history = repeat;
+        return true;
+    } else {
+        repeat_history = repeat;
+        return false;
+    }
+}
+
+// Checks a key in a row to ensure it stays down
+bool debounce_key(uint8_t row)
+{
+    int debounce;
+    for (debounce = 0; debounce < DEBOUNCE_DELAY; debounce++) {
+        _delay_us(10);
+        if (!(read_rows() & _BV(row))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Look up the key in the ROM, with shift and control
+uint8_t key_lookup(uint8_t scancode)
+{
+    uint16_t shift = (PIND & _BV(5)) ? 90 : 0;
+    uint16_t control = (PIND & _BV(4)) ? 180: 0;
+    return key_rom[(uint16_t)scancode + shift + control];
+}
+
+// Scan the keyboard matrix
+bool scan_keys()
+{
+    uint8_t x, y, rom_addr, debounce;
+    uint16_t row_data;
+    bool keydown = false;
+
+    rom_addr = 0;
+    for (x = 0; x < 9; x++) {
+        enable_column(x);
+        _delay_us(ROW_SETTLING_TIME);
+        row_data = read_rows();
+        // Sense logic also uses a counter.
+        for (y = 0; y < 10; y++) {
+            if (row_data & _BV(y)) {
+                if (!keyflags[rom_addr] & debounce_key(y)) {
+                    PORTA = key_lookup(rom_addr);
+                    strobe();
+                    keyflags[rom_addr] = true;  // Key is down, update memory so we don't emit it again
+
+                    // Continue scanning for more keys
+                }
+            } else {
+                keyflags[rom_addr] = false;  // Key is not down, update memory
+            }
+            // Detect if any key is down
+            if (keyflags[rom_addr]) keydown = true;
+            rom_addr++;
+        }
+        disable_column(x);
+    }
+    return keydown;
+}
+
+// Note: Emit strobe pulse for one clock period (around 50us).
+
+void main(void)
+{
+    // Set up ports
+    DDRA = 0xff; // Output port
+    PORTA = 0x00; // No character
+
+    DDRB = 0x00; // Column drivers (X) are off by default
+    PORTB = 0x00; // No pullup resistors for column drivers
+
+    DDRC = 0x00; // Row connections (Y) are inputs
+    PORTC = 0xff; // Pullup resistors turned on
+
+    DDRD = _BV(1) | _BV(3); // SRB and n/c are outputs
+    PORTD = _BV(2) | _BV(6) | _BV(7); // Pullups for REPEAT, Y10 and Y1
+
+    while(1) {
+        if (scan_keys() && is_repeat()) {
+            strobe(); // pulse strobe again
+        }
+    }
+}
